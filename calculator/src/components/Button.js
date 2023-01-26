a3bd09f15e2bb365b0aabb07fc7b002bf4a4aa01
Reaking1@ <reabetsweramoswane@gmail.com>
2023-01-26 18:23:17 +0200

    uupdated

---
+++ calculator/src/components/Button.js
@@ -0,0 +1,11 @@
+import "./Button.css";
+
+const Button = ({className, value, onclick}) => {
+    return (
+        <button className={className} onClick={onclick}>
+            {value}
+        </button>
+    );
+};
+
+export default Button;
